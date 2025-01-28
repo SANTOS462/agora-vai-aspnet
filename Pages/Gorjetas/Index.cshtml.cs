ac58bbb75d073dde2e7ffbf6ead13de54ae9bfc8
Whsantos2013@gmail.com <whsantos2013@gmail.com>
2025-01-28 20:50:38 -0300

    Primeiro commit quebrando tudo

---
+++ Pages/Gorjetas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgoraVai.Pages.Gorjetas
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
